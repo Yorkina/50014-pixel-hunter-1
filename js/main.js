2e5bc12b87eaf4cd1be0ea6decb81c385612846b
Natalia Yorkina <yorkinan@gmail.com>
2017-05-24 02:15:57 +0300

    add right and left navigation

--- js/main.js
+++ js/main.js
@@ -4,24 +4,58 @@ const templates = Array.from(document.querySelectorAll(`template`));
 /** @type {HTMLElement} */
 const mainContainer = document.querySelector(`.central`);
 
+/** @type {HTMLElement} */
+const main = mainContainer.querySelector(`.central__content`);
+
 /** @const {string} */
 const ARROW_RIGHT_KEY_CODE = `ArrowRight`;
 
-/** @type {number} */
-let listCounter = 0;
+/** @const {string} */
+const ARROW_LEFT_KEY_CODE = `ArrowLeft`;
 
-/** @param {number} index */
-const pasteTemplate = (index) => {
-  const content = templates[index].content;
-  const newNode = content.cloneNode(true);
+/** @type {number} */
+let listCounter = -1;
+
+const pasteTemplate = (template = main) => {
   mainContainer.innerHTML = ``;
-  mainContainer.appendChild(newNode);
+  mainContainer.appendChild(template);
+};
+
+const createTemplate = () => {
+  if (listCounter < 0) {
+    pasteTemplate();
+    return;
+  }
+
+  const content = templates[listCounter].content;
+  const newNode = content.cloneNode(true);
+  pasteTemplate(newNode);
+};
+
+const increaseCounter = () => {
+  if (listCounter >= (templates.length - 1)) {
+    return;
+  }
+
+  listCounter++;
+};
+
+const decreaseCounter = () => {
+  if (listCounter < 0) {
+    return;
+  }
+
+  listCounter--;
 };
 
 document.addEventListener(`keydown`, (evt) => {
   if (evt.altKey && evt.key === ARROW_RIGHT_KEY_CODE) {
-    if (listCounter < templates.length) {
-      pasteTemplate(listCounter++);
-    }
+    increaseCounter();
+    createTemplate();
+  }
+
+  if (evt.altKey && evt.key === ARROW_LEFT_KEY_CODE) {
+    decreaseCounter();
+    createTemplate();
   }
 });
