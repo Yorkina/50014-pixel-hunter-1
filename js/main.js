482e041f833620e4ba4db53abc6f6ed034bf3c16
Natalia Yorkina <yorkinan@gmail.com>
2017-05-25 00:11:23 +0300

    fix paste screen/increase/decrease/ and delete extra right/left func

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const mainContainer = document.querySelector(`.central`);
-
 const ARROW_RIGHT_KEY_CODE = `ArrowRight`;
 
 const ARROW_LEFT_KEY_CODE = `ArrowLeft`;
@@ -18,6 +16,7 @@ const templates = [
 ].map((template) => template.content);
 
 const addFragment = () => {
+  const mainContainer = document.querySelector(`.central`);
   const fragment = document.createDocumentFragment();
   const newMain = mainContainer.cloneNode(true);
   fragment.appendChild(newMain);
@@ -26,43 +25,25 @@ const addFragment = () => {
 
 templates.unshift(addFragment());
 
-const pasteScreen = () => {
-  const content = templates[currentActiveScreen];
+const pasteScreen = (content) => {
+  const mainContainer = document.querySelector(`.central`);
   const newNode = content.cloneNode(true);
   mainContainer.innerHTML = ``;
   mainContainer.appendChild(newNode);
 };
 
-const increase = (max, current) => {
-  return Math.min(max, current);
-};
-
-const decrease = (min, current) => {
-  return Math.max(min, current);
-};
-
-const pressRight = () => {
-  const currentScreen = ++currentActiveScreen;
-  currentActiveScreen = increase(templates.length - 1, currentScreen);
-  pasteScreen();
-};
-
-const pressLeft = () => {
-  const currentScreen = --currentActiveScreen;
-  currentActiveScreen = decrease(MIN_SCREEN_PAGE_NUMBER, currentScreen);
-  pasteScreen();
-};
+document.addEventListener(`keydown`, (evt) => {
+  if (evt.altKey && evt.key === ARROW_LEFT_KEY_CODE) {
+    const currentScreen = --currentActiveScreen;
+    currentActiveScreen = Math.max(MIN_SCREEN_PAGE_NUMBER, currentScreen);
+    pasteScreen(templates[currentActiveScreen]);
+  }
+});
 
 document.addEventListener(`keydown`, (evt) => {
-  if (!evt.altKey) {
-    return;
-  }
-
-  if (evt.key === ARROW_RIGHT_KEY_CODE) {
-    pressRight();
-  }
-
-  if (evt.key === ARROW_LEFT_KEY_CODE) {
-    pressLeft();
+  if (evt.altKey && evt.key === ARROW_RIGHT_KEY_CODE) {
+    const currentScreen = ++currentActiveScreen;
+    currentActiveScreen = Math.min(templates.length - 1, currentScreen);
+    pasteScreen(templates[currentActiveScreen]);
   }
 });
