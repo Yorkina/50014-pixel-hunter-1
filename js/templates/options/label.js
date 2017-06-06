4e82819bae99bac2c52e39a528f07d9f76e4edab
Natalia Yorkina <yorkinan@gmail.com>
2017-06-07 00:09:20 +0300

    delete unuseful  file

--- js/templates/options/label.js
+++
@@ -1,5 +0,0 @@
-export default (params) =>
-  `<label class="game__answer game__answer--paint">
-      <input name="question1" type="radio" value="paint">
-      <span>Рисунок</span>
-    </label>`;
